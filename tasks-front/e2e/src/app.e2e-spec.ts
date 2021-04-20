6e89d4cb21250f5dd83c67f6007922f05d401a1f
rachid <olgor@hotmail.com>
2021-04-20 15:25:50 +0200

    frontend changed

--- tasks-front/e2e/src/app.e2e-spec.ts
+++ tasks-front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tasks-mysql app is running!');
+    expect(page.getTitleText()).toEqual('tasks-front app is running!');
   });
 
   afterEach(async () => {
--- tasks-front/src/app/app.module.ts
+++ tasks-front/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
